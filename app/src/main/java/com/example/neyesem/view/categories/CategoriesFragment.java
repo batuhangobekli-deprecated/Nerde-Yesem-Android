150897ec3ae5abc2f4ca22851093c34280da95e7
batuhangobekli <btubatuhan@gmail.com>
2019-05-14 11:16:06 +0300

    MinSdk Version updated for fingerprint

--- app/src/main/java/com/example/neyesem/view/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/neyesem/view/categories/CategoriesFragment.java
@@ -17,7 +17,7 @@ public class CategoriesFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (parentView == null) {
-            
+
         }
         return parentView;
     }
